243a1c8d35b366340bf0d58a79f12da98e75ba80
JavierSolerArtero99 <javier.soler.jsa@gmail.com>
2021-02-03 09:12:19 +0100

    mail DOONE

---
+++ .github/actions/mail_action/index.js
@@ -0,0 +1,34 @@
+const nodemailer = require("nodemailer");
+const core = require("@actions/core");
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: core.getInput("EMAIL_FROM"),
+    pass: core.getInput("PASSWORD"),
+  },
+});
+
+const options = {
+  from: core.getInput("EMAIL_FROM"),
+  to: core.getInput("EMAIL_TO"),
+  subject: "Resultado del workflow ejecutado",
+  text: `REPOSITORIO: ${core.getInput("REPOSITORY")};
+  WORKFLOW NAME: ${core.getInput("NAME_WORKFLOW")} 
+  RAMA: ${core.getInput("BRANCH")} 
+  RESULTADOS:
+  - syntax_check_job: ${core.getInput("SYNTAX")}
+  - test_execution_job: ${core.getInput("TEST")}
+  - build_statics_job: ${core.getInput("BUILD")}
+  - deploy_job: ${core.getInput("DEPLOY")}
+  ACCION REALIZADA POR: ${core.getInput("ACTOR")}
+  OWNER: ${core.getInput("OWNER")}`,
+};
+
+transporter.sendMail(options, function (error, info) {
+  if (!error) {
+    core.setOutput("response", "El mensaje se ha enviado con éxito");
+  } else {
+    core.setFailed(error);
+  }
+});
